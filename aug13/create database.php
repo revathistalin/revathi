75c2764519ff70420f987106d5a71ea126ca4a52
revathistalin <revathi.stalin@kenhike.com>
2018-08-20 11:34:57 +0530

    array function

--- aug13/create database.php
+++ aug13/create database.php
@@ -14,7 +14,11 @@ echo "Connected successfully";
 
 
 // Create database
+<<<<<<< HEAD
 $sql=mysqli_query($conn, "CREATE DATABASE todolist");
+=======
+$sql=mysqli_query($conn, "CREATE DATABASE dbase");
+>>>>>>> 427a875e91eb35c142b517e3c2f02284af85cf01
 if (mysqli_query($conn, $sql)) {
     echo "Database created successfully";
 } else {
@@ -23,10 +27,19 @@ if (mysqli_query($conn, $sql)) {
 
 
 // sql to create table
+<<<<<<< HEAD
 $sql1=mysql_query($conn, "CREATE TABLE listtable (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 name VARCHAR(30) NOT NULL,
 status VARCHAR(30) NOT NULL");
+=======
+$sql1=mysql_query($conn, "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP)");
+>>>>>>> 427a875e91eb35c142b517e3c2f02284af85cf01
 
 
 if (mysqli_query($conn,$sql1)) {
---
+++ aug20/hompage.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+	<table>
+
+<form action="arrayresult.php" method="post">	
+<tr><td>Enter the value<input type="text" name="name"></td></tr>
+<tr><td><input type="submit" name="submit" value="submit"></td></tr>
+</form>
+</table>
+
+
+</body>
+</html>
