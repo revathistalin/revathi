d56c5fc42fe6f45f9e26a7545561dc939c7db528
revathistalin <revathi.stalin@kenhike.com>
2018-08-20 11:55:40 +0530

    using array function

--- aug20/arrayresult.php
+++ aug20/arrayresult.php
@@ -1,7 +1,8 @@
 <?php
-function result()
+function result() //using function
 {
 $name=$_POST['name'];
+//create mutidimensional array
 $carsdtails=array(array("name"=>"volvo","speed"=>"100km/h","price"=>"3lakh"),
            array("name"=>"duster","speed"=>"150km/h","price"=>"4lakh"),
            array("name"=>"suzuki","speed"=>"160km/h","price"=>"6lakh"));
@@ -12,6 +13,7 @@ foreach($carsdtails as $key=>$value)
 {
 	if($value['name']===$name)
 	{
+		//condition is true print cars details
       echo "Name is:" .$value["name"];echo "<br>";
       echo "Value is:" .$value["speed"];echo "<br>";
       echo "Price is:" .$value["price"];echo "<br>";
--- aug20/hompage.php
+++ aug20/hompage.php
@@ -1,7 +1,6 @@
 <html>
 <body>
-	<table>
-
+<table> 
 <form action="arrayresult.php" method="post">	
 <tr><td>Enter the value<input type="text" name="name"></td></tr>
 <tr><td><input type="submit" name="submit" value="submit"></td></tr>
